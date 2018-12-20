ee80abbcfde2cb1d00917f33c7c0528cdc006ad7
Christopher Duenas <chrisduenasonline@gmail.com>
2018-12-20 13:51:03 -0500

    Implmented POST test for valid user

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.post('/', async (req, res) => {
+    let user = { name: 'user1' };
+    res.send(user);
+})
+
 const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 let server;
 
 describe('/', () => {
+
     beforeEach(() => { server = require('../index'); })
     afterEach(() => { server.close(); })
 
@@ -10,6 +11,13 @@ describe('/', () => {
         it('should return Helloworld', async () => {
             const res = await request(server).get('/');
             expect(res.status).toBe(200);
+        });
+    });
+
+    describe('POST /', () => {
+        it('should save if user is valid', async () => {
+            const res = await request(server).post('/');
+            expect(res.body).toHaveProperty('name', 'user1');
         });
     });
 });
