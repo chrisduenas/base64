060ffbb78555d127ba61f9b095fadef7b2165833
Christopher Duenas <chrisduenasonline@gmail.com>
2018-12-20 15:01:13 -0500

    Test implmentation for GET/user & POST/user

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 
-let server;
-
 describe('/', () => {
+    let server;
 
-    beforeEach(() => { server = require('../index'); })
+    beforeEach(() => { server = require('../index');
+    });
+
     afterEach(() => { server.close(); })
 
     describe('GET /', () => {
@@ -14,10 +15,31 @@ describe('/', () => {
         });
     });
 
-    describe('POST /', () => {
+    describe('GET /user', () => {
+        it('should return all users', async() => {
+            const res = await request(server).get('/user');
+            expect(res.status).toBe(200);
+        })
+    })
+
+    describe('POST /user', () => {
+
         it('should save if user is valid', async () => {
-            const res = await request(server).post('/');
-            expect(res.body).toHaveProperty('name', 'user1');
+            const res = await request(server)
+                            .post('/user')
+                            .send({
+                                id: 1,
+                                name: 'user1'});
+            
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('id', 1);
+            expect(res.body).toHaveProperty('name','user1');
         });
+
+        it('should not save if user is not valid', async () => {
+            user = '';
+            const res = await request(server).post('/');
+            expect(res.status).toBe(404);
+        })
     });
 });
