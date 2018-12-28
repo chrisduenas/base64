7607a98b8986f9ac538c9a6a74e5c10227eddbdc
Christopher Duenas <chrisduenasonline@gmail.com>
2018-12-28 15:29:45 -0500

    Implemented User PUT route to pass tests

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const users = [
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
 app.get('/user', (req, res) => {
     res.send(users);
-})
+});
 
 app.post('/user', (req, res) => {
     const user = {
@@ -24,6 +24,17 @@ app.post('/user', (req, res) => {
     };
     if (!req.body.name) return res.status(404).send('User not found');
     users.push(user);
+    res.send(user);
+});
+
+app.put('/user/:id', (req, res) => {
+    const user = {
+        id: req.body.id,
+        name: req.body.name
+    };
+    if(!req.body.name) return res.status(404).send('User cannot be updated');
+    if(req.body.name.length < 5) return res.status(400).send('Name must be at least 5 characters');
+
     res.send(user);
 })
 
