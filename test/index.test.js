24c70b1c64a22227fdbf5b0f01c0241d8e6c86b7
Christopher Duenas <chrisduenasonline@gmail.com>
2019-01-03 15:34:13 -0500

    Refactored test suites for user route

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-describe('/', () => {
+describe('/user', () => {
     let server;
 
     beforeEach(() => { server = require('../index');
@@ -9,28 +9,25 @@ describe('/', () => {
     afterEach(() => { server.close(); })
 
     describe('GET /', () => {
-        it('should return Helloworld', async () => {
-            const res = await request(server).get('/');
-            expect(res.status).toBe(200);
-        });
-    });
-
-    describe('GET /user', () => {
         it('should return all users', async() => {
             const res = await request(server).get('/user');
             expect(res.status).toBe(200);
         })
     })
 
-    describe('POST /user', () => {
+    describe('POST /', () => {
         let name;
+
+        const exec = async () => {
+            return request(server)
+                    .post('/user')
+                    .send({name});
+        }
 
         it('should save if user is valid', async () => {
             name = 'user4';
 
-            const res = await request(server)
-                            .post('/user')
-                            .send({name});
+            const res = await exec();
             
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('id', 4);
@@ -39,23 +36,23 @@ describe('/', () => {
 
         it('should return 400 if user is less than 3 characters', async () => {
             name = "12";
-            const res = await request(server).post('/user').send();
+            const res = await exec();
             expect(res.status).toBe(400);
         });
 
         it('should return 400 if user is more than 20 characters', async() => {
             name = new Array(25).join('a');
-            const res = await request(server).post('/user').send({name});
+            const res = await exec();
             expect(res.status).toBe(400);
         });
     });
 
-    describe('GET /user/:id', () => {
+    describe('GET /:id', () => {
         let id;
 
         it('should return 404 if user with given id is not found', async() => {
             id = '';
-            const res = await request(server).get('/user/:id').send({id});
+            const res = await request(server).get('/user/:id');
             expect(res.status).toBe(404);
         });
 
@@ -65,26 +62,35 @@ describe('/', () => {
         });
     });
 
-    describe('PUT /user/:id', () => {
+    describe('PUT /:id', () => {
         let newId;
         let newName;
 
-        it('should return 404 if id is not valid', async () => {
-            newId = 4;
-            const res = await request(server)
-                            .put('/user/:id')
-                            .send({id: newId});
+        const exec = () => {
+            return request(server)
+                    .put('/user/' + newId)
+                    .send({ 
+                            id: newId,
+                            name: newName
+                            });
+        }
+
+        beforeEach( async() => {
+            newId = 1;
+        });
+
+        it('should return 404 if ID is not valid', async () => {
+            newId = '';
+
+            const res = await exec();
 
             expect(res.status).toBe(404);
         });
 
         it('should return 400 if user name is less than 3 characters', async () => {
-            newId = '4';
             newName = '12'
 
-            const res = await request(server)
-                            .put('/user/' + newId)
-                            .send({name: newName});
+            const res = await exec();
 
             expect(newName).toHaveLength(2);
             expect(res.status).toBe(400);
@@ -93,9 +99,7 @@ describe('/', () => {
         it('should return 400 if user name is more than 20 characters', async () => {
             newName = new Array(22).join('a');
 
-            const res =  await request(server)
-                            .put('/user/' + newId)
-                            .send({name: newName});
+            const res =  await exec();
 
             expect(newName).toHaveLength(21);
             expect(res.status).toBe(400);
@@ -103,24 +107,29 @@ describe('/', () => {
 
     });
 
-    describe('DELETE /user/:id', () => {
-        let user;
+    describe('DELETE /:id', () => {
+        let name;
+        let id;
+         
+        const exec = async () => {
+            return await request(server).delete('/user/' + id);
+        }
+
+        beforeEach( async() => {
+            id = 1,
+            name = 'user1'
+        });
 
         it('should return 404 if id is not valid', async () => {
             id = '';
 
-            const res = await request(server).delete('/user/' + id);
+            const res = await exec();
 
             expect(res.status).toBe(404);
         });
 
         it('should return the removed user', async () => {
-            user = {
-                "id" : 1,
-                "name" : "user1"
-            }
-
-            const res = await request(server).delete('/user/' + user.id);
+            const res = await exec();
 
             expect(res.body).toHaveProperty('id', 1);
             expect(res.body).toHaveProperty('name', 'user1');
